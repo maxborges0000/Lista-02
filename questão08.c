70fc3466e6f975daf175748601d2d05a49163a64
maxborges0000 <47908452+maxborges0000@users.noreply.github.com>
2019-06-12 09:58:17 -0300

    Add files via upload

---
+++ questão08.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int converte_binario(int n){
+	int temp,cont=0,bin[50];
+	while(n!=1){
+	    temp=n%2;
+	    n=n/2;
+	    bin[cont]=temp;
+	    cont++;
+
+	}
+	bin[cont]=1
+	for(int c=9;c>=0;c--){
+	    printf("%i",bin[c]);
+
+
+}
+
+	return 0;
+
+}
+
+n*pow
+
+void main(){
+
+	bin[]
+
+
+}
